cce2b3f3883d76d6bd21cd6885617da275218af0
tovad <tovad@LAPTOP-8IO9EOND>
2024-09-04 12:53:48 +0200

    Add project files.

---
+++ How to github/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace How_to_github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
